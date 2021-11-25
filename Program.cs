a8ab72a80e228e9f08f2455fa5aad778835c24f6
HUSNAIN <ranasunain584@gmail.com>
2021-11-24 19:09:59 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name, surname;
+            
+
+            console.writeline("enter your name");
+            console.readline();
+
+            console.writeline("enter your surname");
+            console.readline();
+
+
+            console.Readkey();
+        }
+    }
+}
